1b2658c9f5e80a3a64ec6fa951453b73ef796c28
Blessed Panashe Nyathi <bnyathi80@yahoo.com>
2026-05-02 10:41:34 +0200

    Initial commit: engine skeleton, CMake, gitignore

---
+++ engine-cpp/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+
+    std::cout << "LithiSim-Alpha engine (C++)\n";
+    std::cout << "Build with CMake, then run the lithisim executable.\n";
+    return 0;
+}
